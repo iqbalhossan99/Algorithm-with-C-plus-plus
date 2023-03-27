c75b663795a420c0fa0a63a9c2e233230c8aa880
MD IQBAL HOSSAN <engiqbal1999@gmail.com>
2023-03-28 01:04:18 +0600

    path print using bfs in a graph

--- week-03-problem-solving-with-BFS-&-DFS/ml-11-lab-ass-01/2.SSSP-Using-BFS.cpp
+++ week-03-problem-solving-with-BFS-&-DFS/ml-11-lab-ass-01/2.SSSP-Using-BFS.cpp
@@ -1,1 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int n =  1e5;
 
+ vector<int> adj_list[n];
+
+int main()
+{
+          int nodes, edges;
+          cin>>nodes>>edges;
+
+
+
+          int u, v;
+          for(int i=0; i<edges; i++)
+          {
+                    cin>>u>>v;
+                    adj_list[u].push_back(v);
+                    adj_list[v].push_back(u);
+          }
+
+          for(int i=0; i<edges; i++)
+          {
+                    for(int j=0; j<adj_list[i].size(); j++)
+                     cout<<adj_list[i][j]<<" ";
+
+                    cout<<endl;
+          }
+
+
+
+
+
+
+    return 0;
+}
